807808d71c9f3a0232ba5ad876f34e2c0c308577
Shital95610 <117344821+Shital95610@users.noreply.github.com>
2026-06-02 06:05:58 +0530

    Create HR-Management-System-SpringbootApplication.java

---
+++ HR-Management-System-SpringbootApplication.java
@@ -0,0 +1,11 @@
+package com.hr.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrManagementSystemSpringbootApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HrManagementSystemSpringbootApplication.class, args);
+    }
+}
