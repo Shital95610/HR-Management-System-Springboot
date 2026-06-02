db536bc0035918bc369638894050806cd3ef6c7a
Shital95610 <117344821+Shital95610@users.noreply.github.com>
2026-06-02 05:48:51 +0530

    LastDigitFromNumber with EmployeeController

--- LastDigitFromNumber.java
+++ LastDigitFromNumber.java
@@ -1,20 +1,48 @@
-package basicprogram;
+package com.hr.management.controller;
 
-public class LastDigitFromNumber {
-public static void main(String[] args){
-		{
-			int number=3456;
-			int firstDigit=3;
-			int lastDigit=6;
-		
-			lastDigit=number%10;
-			System.out.println("Last digit: "+lastDigit);
-			
-			while(number!=0) {
-				firstDigit=number%10;
-				number /=10;
-			}
-		System.out.println("First Digit: "+firstDigit);
-		}
-}
+import com.hr.management.model.Employee;
+import com.hr.management.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/employees")
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    // 1. Get All Employees - GET API
+    @GetMapping
+    public ResponseEntity<List<Employee>> getAllEmployees() {
+        return ResponseEntity.ok(employeeService.getAllEmployees());
+    }
+
+    // 2. Get Employee by ID - GET API  
+    @GetMapping("/{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id) {
+        return ResponseEntity.ok(employeeService.getEmployeeById(id));
+    }
+
+    // 3. Add New Employee - POST API
+    @PostMapping
+    public ResponseEntity<Employee> addEmployee(@RequestBody Employee employee) {
+        return ResponseEntity.ok(employeeService.saveEmployee(employee));
+    }
+
+    // 4. Update Employee - PUT API
+    @PutMapping("/{id}")
+    public ResponseEntity<Employee> updateEmployee(@PathVariable Long id, @RequestBody Employee employee) {
+        return ResponseEntity.ok(employeeService.updateEmployee(id, employee));
+    }
+
+    // 5. Delete Employee - DELETE API
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteEmployee(@PathVariable Long id) {
+        employeeService.deleteEmployee(id);
+        return ResponseEntity.ok("Employee deleted successfully");
+    }
 }
