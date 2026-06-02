8b2743b62407c2fa05ecca08b389a61871d171ad
Shital95610 <117344821+Shital95610@users.noreply.github.com>
2026-06-02 05:59:29 +0530

    Create EmployeeRepository.java

---
+++ EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.hr.management.repository;
+
+import com.hr.management.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
